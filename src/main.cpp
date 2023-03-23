11512ed07ebfc51373d358b43c1a44fe70c1472c
chippomueller <chippo@goodwoodaudio.com>
2023-03-23 11:23:40 +1100

    NotWorking

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <EEPROM.h>
 
 Bounce ftswa = Bounce();
-Bounce ftswb = Bounce();
 
 Bypass bypass;
 
@@ -18,31 +17,19 @@ void setup() {
   ftswa.attach(FTSWA_PIN);
   ftswa.interval(40);
 
-  pinMode(FTSWB_PIN, INPUT_PULLUP);
-  ftswb.attach(FTSWB_PIN);
-  ftswb.interval(40);
-
   bypass.Init();
-
 }
 
 void loop(){
 
   ftswa.update();            // poll inputs every loop
-  ftswb.update();
+  bypass.pressType();
 
-  if (ftswa.fell()) {
-    bypass.ToggleMasterState();
+//  if (ftswa.fell()) {
+//    bypass.ToggleMasterState();
 //    #ifdef __DEBUG__
 //        Serial.println("footswitch a fell");
 //    #endif
-  }
-
-  if (ftswb.fell()) {
-    bypass.ToggleDryState();
-//    #ifdef __DEBUG__
-//        Serial.println("footswitch b fell");
-//    #endif
-  }
+//  }
 }
 
