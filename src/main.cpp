892185f18f569d52c0e831bfc48bdf57e27b2141
Chippo <106004510+ChippoMueller@users.noreply.github.com>
2023-05-15 20:21:35 +1000

    Final_Tap_Working

--- src/main.cpp
+++ src/main.cpp
@@ -8,33 +8,44 @@
 
 const int BUTTON_PIN = 8;
 const int LED_PIN = 7;
-const int SAMPLE_SIZE = 1;
+const int SAMPLE_SIZE = 4;
 
 const unsigned long LIGHT_ON_INTERVAL = 50;                    // milliseconds that light should be on for
 
 // when blinking
 
-const unsigned long MAX_FLASH_INTERVAL = 1500;             // maximum interval time  500 millis  = 120 bpm
-const unsigned long BOOT_FLASH_INTERVAL = 1000;             // flash rate in milliseconds at bootup
+const unsigned long MAX_TAP_INTERVAL = 1500;             // maximum interval time  500 millis  = 120 bpm
+const unsigned long BOOT_TAP_INTERVAL = 1000;             // Tap rate in milliseconds at bootup
 
 //                                            1000 millis  = 60   bpm
 //                                            1500 millis  = 40   bpm
 //                                            2000 millis  = 30   bpm
 
-int buttonState = HIGH;
-int lastButtonState = HIGH;
+//int buttonState = HIGH;
+//int lastButtonState = HIGH;
 unsigned long lastDebounceTime = 0;
 unsigned long debounceDelay = 50;
-unsigned long flashInterval = BOOT_FLASH_INTERVAL;         // The interval at bootup of between button presses
-float bpm = 60000.0 / BOOT_FLASH_INTERVAL;
+unsigned long tapInterval = BOOT_TAP_INTERVAL;         // The interval at bootup of between button presses
+int bpm = int(60000.0 / BOOT_TAP_INTERVAL);          // starting BPM value
+float averageTapInterval;     // The average tap interval as a fraction
+
+unsigned long intervalArray[SAMPLE_SIZE];     // Array of intervals between taps
 
 unsigned long prevMillis = 0;
 unsigned long currMillis = 0;
 unsigned long intervalStartMillis = 0;
 
-bool flashIntervalStart = 1;                                                    // Controls when light goes on when blinking
+int tapCount = 0;             // Keep count of taps 
 
-Bounce ftsw = Bounce();                                            // ftsw is an instance of a BOUNCE Object
+bool tapIntervalStart = 1;    // Controls when light goes on when blinking
+
+Bounce ftsw = Bounce();       // ftsw is an instance of a BOUNCE Object
+
+void ControlFlashing(void);
+void ControlButtonPress(void);
+void InitialiseIntervalArray(void);
+void UpdateIntervalArray(unsigned long tapInterval, int tapIndex);
+float CalculateAvgInterval();
 
 void setup() {
 
@@ -54,30 +65,25 @@ void loop() {
 
 ftsw.update();                                                   // Update the Bounce instance ???
 
-void testFunction(void);
-
-//  ***  There are three button states:   reading, buttonState and lastButtonState ???  ***
-
-//  ***  I’m sure we can get by with just buttonState and lastButtonState                        ***
+ControlFlashing();
+ControlButtonPress();
                                                                               
-testFunction();
-                                                        // finish Main Loop
 }
 
-void testFunction(){
+void ControlFlashing(void){
 
-// Main test to control blinking
+// Main test to control flashing
  
 
-if (flashInterval > 0) {
+if (tapInterval > 0) {
 
  
 
-      if (flashIntervalStart == 1) {
+      if (tapIntervalStart == 1) {
 
                  digitalWrite(LED_PIN, HIGH);              // Interval Start - Turn light on
 
-                 flashIntervalStart = 0;
+                 tapIntervalStart = 0;
 
  
 
@@ -87,16 +93,17 @@ if (flashInterval > 0) {
 
      
 
-       if (millis() >= intervalStartMillis + flashInterval) {     // interval complete repeat blinking
+       if (millis() >= intervalStartMillis + tapInterval) {     // interval complete repeat blinking
 
                  intervalStartMillis = millis();
 
-                 flashIntervalStart = 1;
+                 tapIntervalStart = 1;
       }
   }
 }
+}
 
- 
+void ControlButtonPress(void){
 
 // Detect button state change and if preesed calculate a new blinking interval
 
@@ -107,206 +114,80 @@ if (flashInterval > 0) {
 
       currMillis = intervalStartMillis = millis();
 
-     
+         if ((currMillis - prevMillis) < MAX_TAP_INTERVAL) {          // Anything greater than max tap interval is too slow in bpm
 
-      if ( prevMillis != 0 ) {
+         unsigned long currInterval = currMillis - prevMillis;        // Calculate the tap interval
+         UpdateIntervalArray(currInterval, tapCount % SAMPLE_SIZE);   // Update the array of intervals
+         averageTapInterval = CalculateAvgInterval();                 // Tap interval for BPM calculation
+         tapInterval = long (averageTapInterval + .5);                // Rounded tap interval for flashing light 
+         tapCount++;                                                  // Increment tap count
+         tapIntervalStart = 1;                                        // Start the flashing process, otherwise leaving flashing as is
+         bpm = int((60000.0 / averageTapInterval) + .5);              // Calculate beats per minute
 
-        
+                 tapInterval = currMillis - prevMillis;
 
-         if ((currMillis - prevMillis) < MAX_FLASH_INTERVAL) {          // Anything greater than max flash interval is too slow in bpm
-
-                 flashInterval = currMillis - prevMillis;
-
-                 flashIntervalStart = 1;                                                             // Start the flashing process, otherwise leave flashing as is
-
+              #ifdef __DEBUG__
                  Serial.print("Accepted interval: ");                                                      // Debugging
+              #endif
 
-           }  else {
+           }  else {              // Considered to be the first tap
 
-                    Serial.print("Rejected interval too large: ");                          // Debugging
+          InitialiseIntervalArray();        // Fill interval array with zeros
+          tapCount = 0;                         // Reset tap counter
 
+              #ifdef __DEBUG__
+                    Serial.print("Rejected interval too large: ");          // Debugging
+              #endif
       }
 
-         
+        #ifdef __DEBUG__
+           Serial.print("BPM: ");               // Debugging
+           Serial.println(bpm);      
+        #endif 
 
-           bpm = 60000.0 / flashInterval;
-
-         
-           Serial.print("BPM: ");                                                                        // Debugging - love this, good boy!!!
-           Serial.println(bpm);       
-
-    }   
         prevMillis = currMillis;   
-}
+    }
   }   
 }
 
-
-
-
-
-
-/*
-// Tap Tap Program
-
-#include <Arduino.h>
-#include <Bounce2.h>
-
- #define __DEBUG__   // comment out when not using Uno
-
-
-const int BUTTON_PIN = 8;
-const int LED_PIN = 7;
-const int SAMPLE_SIZE = 1;
-
-const unsigned long LIGHT_ON_INTERVAL = 50;                    // milliseconds that light should be on for
-
-// when blinking
-
-const unsigned long MAX_FLASH_INTERVAL = 1500;             // maximum interval time  500 millis  = 120 bpm
-const unsigned long BOOT_FLASH_INTERVAL = 1000;             // flash rate in milliseconds at bootup
-
-//                                            1000 millis  = 60   bpm
-//                                            1500 millis  = 40   bpm
-//                                            2000 millis  = 30   bpm
-
-int buttonState = HIGH;
-int lastButtonState = HIGH;
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;
-unsigned long flashInterval = BOOT_FLASH_INTERVAL;         // The interval at bootup of between button presses
-float bpm = 60000.0 / BOOT_FLASH_INTERVAL;
-
-unsigned long prevMillis = 0;
-unsigned long currMillis = 0;
-unsigned long intervalStartMillis = 0;
-
-bool flashIntervalStart = 1;                                                    // Controls when light goes on when blinking
-
-Bounce ftsw = Bounce();                                            // ftsw is an instance of a BOUNCE Object
-
-void setup() {
-
-  #ifdef __DEBUG__
-      Serial.begin(9600);
-  #endif 
-
-  pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(LED_PIN, OUTPUT);
-
-  ftsw.attach(BUTTON_PIN);                                // Attach the debouncer to a pin
-  ftsw.interval(40);                                      // Use a debounce interval of 40 milliseconds
-
-}
-
-void loop() {
-
-ftsw.update();                                                   // Update the Bounce instance ???
-
-void testFunction(void);
-
-//  ***  There are three button states:   reading, buttonState and lastButtonState ???  ***
-
-//  ***  I’m sure we can get by with just buttonState and lastButtonState                        ***
-                                                                              
-testFunction();
-                                                        // finish Main Loop
-}
-
-void testFunction(){
-  int reading = ftsw.read();                                      // read the state of the footswitch (HIGH or LOW). Must include debounce time 40 milliseconds
-
-//if (reading != lastButtonState) {                        // footswitch has changed state - reset lastDebounceTime
-//    lastDebounceTime = millis();
-//}
-
-//if (ftsw.fell()) {                        // footswitch has changed state - reset lastDebounceTime
-//    lastDebounceTime = millis();
-//}
-
-// Main test to control blinking
- 
-
-if (flashInterval > 0) {
-
- 
-
-      if (flashIntervalStart == 1) {
-
-                 digitalWrite(LED_PIN, HIGH);              // Interval Start - Turn light on
-
-                 flashIntervalStart = 0;
-
- 
-
-      } else if (millis() >= intervalStartMillis + LIGHT_ON_INTERVAL) {     // lights been on long enough
-
-                 digitalWrite(LED_PIN, LOW);            // Turn light off
-
-     
-
-       if (millis() >= intervalStartMillis + flashInterval) {     // interval complete repeat blinking
-
-                 intervalStartMillis = millis();
-
-                 flashIntervalStart = 1;
-      }
+void InitialiseIntervalArray(void) {
+  // Initialise the interval array with zeros 
+  
+  for (int i = 0; i < SAMPLE_SIZE; i++){
+    intervalArray[i] = 0;
   }
 }
 
- 
+void UpdateIntervalArray(unsigned long tapInterval, int tapIndex){
+  intervalArray[tapIndex] = tapInterval;
+}
 
-// Detect button state change and if preesed calculate a new blinking interval
+float CalculateAvgInterval(){
+  // Need to process first iterations trhu the loop till array fills
+  // The 1st time through, the array will contain [interval, 0, 0, 0]
+  // 2nd time [interval, interval, 0, 0]
+  // 3rd time [interval, interval, interval, 0]
+  // thru to [last intervals, 2nd last interval, 3rd last interval, 4th last interval]
 
- 
+  // calculate the average BPM
+  unsigned long sum = 0;
+  int arrayElements = 0;
+  float avgIntervals;
 
-  if ((millis() - lastDebounceTime) > debounceDelay) {            // a specified time has passed (50 milliseconds)
-    if (reading != buttonState) {                                 // update last button state to current button state
-     
+  for (int i = 0; i < SAMPLE_SIZE; i++){
+    if (intervalArray[i] != 0) {
+      sum += intervalArray[i];
+      arrayElements++;
+    }
+    else {
+      i = SAMPLE_SIZE;
+    }
+  }
 
-      buttonState = reading;
-
-      if (buttonState == LOW) {                                                     // current state is high so footswitch has been pressed
-
-           // calculate the interval between button presses and keep time button pressed
-
-      currMillis = intervalStartMillis = millis();
-
-     
-
-      if ( prevMillis != 0 ) {
-
-        
-
-         if ((currMillis - prevMillis) < MAX_FLASH_INTERVAL) {          // Anything greater than max flash interval is too slow in bpm
-
-                 flashInterval = currMillis - prevMillis;
-
-                 flashIntervalStart = 1;                                                             // Start the flashing process, otherwise leave flashing as is
-
-                 Serial.print("Accepted interval: ");                                                      // Debugging
-
-           }  else {
-
-                    Serial.print("Rejected interval too large: ");                          // Debugging
-
-      }
-
-         
-
-           bpm = 60000.0 / flashInterval;
-
-         
-           Serial.print("BPM: ");                                                                        // Debugging - love this, good boy!!!
-           Serial.println(bpm);       
-
-    }   
-        prevMillis = currMillis;   
+  if (arrayElements > 0) {      // Avoid divisions by zero
+   avgIntervals = (float(sum) / float(arrayElements));   // Calculate average intervals from intervals array
+   return (avgIntervals);
+  } else {
+  return (tapInterval);
   }
 }
-  lastButtonState = reading;
-
-  }   
-}
-
-*/
